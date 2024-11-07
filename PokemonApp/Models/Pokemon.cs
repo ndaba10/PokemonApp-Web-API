666f005c660fe9f7aff6c9ce03667294475314ee
ndaba10 <142088587+ndaba10@users.noreply.github.com>
2024-11-07 19:02:59 +0200

    2nd day

--- PokemonApp/Models/Pokemon.cs
+++ PokemonApp/Models/Pokemon.cs
@@ -5,5 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+        public ICollection<PokemonOwner> PokemonOwner { get; set; }
+        public ICollection<PokemonCategory> PokemonCategory { get; set; }
     }
 }
---
+++ PokemonApp/Models/PokemonCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonApp.Models
+{
+    public class PokemonCategory
+    {
+        public int PokemonId { get; set; }
+        public int CategoryID { get; set; }
+        public Pokemon Pokemon { get; set; }
+        public Category Category { get; set; }
+    }
+}
